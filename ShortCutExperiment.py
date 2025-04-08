86769dc268d852934c38506a81d1c82026fc37cf
L-Agbaria <163034311+L-Agbaria@users.noreply.github.com>
2025-04-08 13:28:16 +0200

    Results and code

--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -36,6 +36,7 @@ def run_repetitions(agent_type, env_type, n_rep=100, n_episodes=1000, e=0.1, a=0
 
     # Print an example of the greedy policy for one iteration, then return
     print("Greedy policy for", agent_type.__name__, "in a", env_type.__name__)
+    # # Add print for parameters
     env.render_greedy(agent.Q)
     smooth_rewards = savgol_filter(total_rewards, 38, 1)
     return np.mean(smooth_rewards, axis=0)
@@ -62,11 +63,31 @@ def graph(curve: np.typing.ArrayLike, label: str, title: str = None, x_label="Ep
     plt.legend(loc='lower right')
 
 
-def run_experiment(default=True, alpha: list = None, n: list = None, agent_types: list = None, env_types: list = None,
-                   seed: int = None, **args):
+def complete_run(agent, env, avg_rewards: list, var: str, val: float, all_rewards: list, all_labels: list, **args):
+    """
+    Saves the run results and outputs the run results figure.
+
+    :param agent: Agent of the run experiment.
+    :param env: Environment of the agent.
+    :param avg_rewards: Averaged results from multiple repetitions.
+    :param var: Name of experimented variable for printing.
+    :param val: Value of experimented variable for printing.
+    :param all_rewards: List containing all current set-up saved results, modified by reference.
+    :param all_labels: List containing all cuurent set-up saved labels, modified by reference.
+    """
+    
+    # Save results
+    all_rewards.append(avg_rewards)
+    all_labels.append(f"{agent.__name__} in {env.__name__}, {var}={val}")
+    # Present results of this specific experiment alone
+    plt.figure()
+    graph(avg_rewards, f"{var}: {val}", f"{agent.__name__} in {env.__name__}")
+
+
+def experiment(alpha: list = None, n: list = None, agent_types: list = None, env_types: list = None, seed: int = None, **args):
     """
     By default, runs all the report required experiments and presents the results for every desired figure.
-    Can be used to run specific experiments on alpha values.
+    Can be used to run specific experiments on alpha or n_step values.
 
     :param default: Runs default experiment
     :param alpha: All alpha float values to be tested
@@ -82,15 +103,15 @@ def run_experiment(default=True, alpha: list = None, n: list = None, agent_types
         np.random.seed(seed)
 
     # Default alpha and n-step experiment ranges
-    if default or alpha is None:
+    if alpha is None:
         alpha = [0.01, 0.1, 0.5, 0.9]
-    if default or n is None:
+    if n is None:
         n = [1, 2, 5, 10, 25]
 
     # By default use all agents and environments
-    if default or agent_types is None:
+    if agent_types is None:
         agent_types = [QLearningAgent, SARSAAgent, ExpectedSARSAAgent, nStepSARSAAgent]
-    if default or env_types is None:
+    if env_types is None:
         env_types = [ShortcutEnvironment, WindyShortcutEnvironment]
 
     # Run repetitions and present immediate results
@@ -100,29 +121,19 @@ def run_experiment(default=True, alpha: list = None, n: list = None, agent_types
             all_rewards = []
             all_labels = []
             # Windy environment only for SARSAAgent
-            if default and agent is not agent_types[1] and env is env_types[1]:
+            if agent is not SARSAAgent and env is WindyShortcutEnvironment:
                 continue
-            # Don't test alpha for nStepsSARSAAgent
-            if default and agent is not agent_types[3]:
+            # Don't test alpha for nStepsSARSAAgent, test n_steps instead
+            if agent is not nStepSARSAAgent:
                 for a in alpha:
                     avg_rewards = run_repetitions(agent, env, a=a, **args)
-                    # Save results
-                    all_rewards.append(avg_rewards)
-                    all_labels.append(f"{agent.__name__} in {env.__name__}, alpha={a}")
-                    # Present results of this specific experiment alone
-                    plt.figure()
-                    graph(avg_rewards, f"Alpha: {a}", f"{agent.__name__} in {env.__name__}")
-            else:  # Should be modified to improve code readability and sustainability
+                    complete_run(agent, env, avg_rewards, "Alpha", a, all_rewards, all_labels)
+            else:
                 for steps in n:
-                    avg_rewards = run_repetitions(agent, env, a=0.9, n_steps=steps, **args)
-                    # Save results
-                    all_rewards.append(avg_rewards)
-                    all_labels.append(f"{agent.__name__} in {env.__name__}, steps={steps}")
-                    # Present results of this specific experiment alone
-                    plt.figure()
-                    graph(avg_rewards, f"Steps: {steps}", f"{agent.__name__} in {env.__name__}")
+                    avg_rewards = run_repetitions(agent, env, a=0.5, n_steps=steps, **args)
+                    complete_run(agent, env, avg_rewards, "Steps", steps, all_rewards, all_labels)
 
-            # Show all results in one figure
+            # Show all results of environment in one figure
             plt.figure()
             for curve, label in zip(all_rewards, all_labels):
                 graph(curve, label, "Results of all experiment variations")
@@ -130,4 +141,17 @@ def run_experiment(default=True, alpha: list = None, n: list = None, agent_types
 
 
 if __name__ == "__main__":
-    run_experiment(n_rep=10)
+    # QLearningAgent
+    experiment(n=[], agent_types=[QLearningAgent], env_types=[ShortcutEnvironment])
+
+    # SARSAAgent
+    experiment(n=[], agent_types=[SARSAAgent], env_types=[ShortcutEnvironment, WindyShortcutEnvironment])
+
+    # ExpectedSARSAAgent
+    experiment(n=[], agent_types=[ExpectedSARSAAgent], env_types=[ShortcutEnvironment])
+
+    # nStepsSARSAAgent
+    experiment(alpha=[], agent_types=[nStepSARSAAgent], env_types=[ShortcutEnvironment])
+
+    # # All experiments, 10 repetitions
+    # experiment(n_rep=10)
